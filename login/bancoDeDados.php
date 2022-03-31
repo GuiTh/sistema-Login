496ec43adaa9caf316946e646b3e1f6d034308e0
Guilherme Carlos <56705014+GuiTh@users.noreply.github.com>
2022-03-31 15:17:43 -0300

    Add files via upload

---
+++ login/bancoDeDados.php
@@ -0,0 +1,10 @@
+<?php
+
+class PDO extends PDO{
+
+    
+}
+
+
+
+?>
---
+++ login/dados.php
@@ -0,0 +1,10 @@
+<?php
+$username = POST['username'] //variavel pega o name do input de login do usuario
+$senha = POST['password'] //variavel pega o name do input de senha do usuario
+if ($username == true){
+}
+
+?>
+
+
+
